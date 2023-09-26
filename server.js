200ca6bc79827a1409b40c6fd54a3e1f82278e84
Deloof Emile <emile.deloof@leerling.pti.be>
2023-09-26 16:10:56 +0200

    added some sort of DB

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require("express")
+const fs = require("fs")
 const PORT = 5001   
 const app = express()
+const DBPath = "./database/database.json"
+
+let DBStructure = {
+    attempts: 134,
+    attemptsMade: 129,
+    fgPercentage: 0 
+}
+DBStructure.fgPercentage = parseInt(DBStructure.attemptsMade) / parseInt(DBStructure.attempts) * 100
+
+fs.readFileSync(DBPath, (e, data) => {
+    if(e) throw e
+    console.log(JSON.stringify(data))
+})
 
 app.use(express.static("views/statics"))
 app.set("view engine", "ejs")
